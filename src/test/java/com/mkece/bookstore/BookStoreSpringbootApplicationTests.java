60e0855b960bd1259f2ff737e3f9d1b49d04b8ab
mithun003 <mk.majumdar003@gmail.com>
2023-09-17 21:20:45 +0530

    adding the jacoco plugin in pom.xml

--- src/test/java/com/mkece/bookstore/BookStoreSpringbootApplicationTests.java
+++ src/test/java/com/mkece/bookstore/BookStoreSpringbootApplicationTests.java
@@ -1,13 +1,22 @@
 package com.mkece.bookstore;
 
 import org.junit.jupiter.api.Test;
+import org.springframework.boot.SpringApplication;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.context.ConfigurableApplicationContext;
+
+import static org.junit.jupiter.api.Assertions.assertNotNull;
 
 @SpringBootTest
 class BookStoreSpringbootApplicationTests {
 
 	@Test
 	void contextLoads() {
+		// Attempt to run the Spring Boot application and capture the context
+		ConfigurableApplicationContext context = SpringApplication.run(BookStoreSpringbootApplication.class);
+
+		// Check that the context is not null, indicating successful startup
+		assertNotNull(context);
 	}
 
 }
